9bdd6e883ba8e7633c40b73fdcaf5773f3af059a
kodunaiff <kodunaiff@gmail.com>
2025-06-26 14:28:25 +0800

    change docker-compose

--- backend/todo_list/settings.py
+++ backend/todo_list/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todolistdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',  # Имя сервиса PostgreSQL в docker-compose
+        'PORT': '5432',
     }
 }
 
