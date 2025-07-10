b94ba39a519dd0d76f813cb4075038f4ebbbbc48
kodunaiff <kodunaiff@gmail.com>
2025-07-10 11:51:26 +0800

    reg user

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,17 +1,34 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Task, Category, User
 from .serializers import TaskSerializer, CategorySerializer, UserSerializer
+from rest_framework.response import Response
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def create_or_get_user(self, telegram_id, username=None):
+    def create(self, request, *args, **kwargs):
+        telegram_id = request.data.get('telegram_id')
+        username = request.data.get('username', None)
+
+        if not telegram_id:
+            return Response(
+                {"error": "telegram_id is required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         user, created = User.objects.get_or_create(
             telegram_id=telegram_id,
             defaults={'username': username}
         )
-        return user
+
+        if created:
+            serializer = self.get_serializer(user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            serializer = self.get_serializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
